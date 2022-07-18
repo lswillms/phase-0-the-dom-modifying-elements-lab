ef157c46ff08d5e9f2f958556f079a515b829748
Latoshia Williams <lswillms@yahoo.com>
2022-07-18 15:12:07 -0500

    commit tree 52eb93aa479b253e0bb4fff26fad497b20bfe568

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const remove  = document.querySelector("main")
+remove.remove()
+
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Latoshia is the champion"
+document.body.append(newHeader)
+
+
+ 
+
+
+
